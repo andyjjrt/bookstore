63db12a6671bc0eb8f31cf63bd789820fdd34bee
andyjjrt <andyjjrt@DESKTOP-SR7H9AR>
2022-02-27 22:08:19 +0800

    Add project files.

---
+++ bookstore/Controllers/BookController.cs
@@ -0,0 +1,111 @@
+﻿using bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+
+        // GET: Index
+        public ActionResult Index()
+        {
+            List<Book> books = new List<Book>();
+            string[] files = Directory.GetFiles(Server.MapPath("~/App_Data"), "*.txt", SearchOption.TopDirectoryOnly);
+            foreach (string item in files)
+            {
+                string[] lines = System.IO.File.ReadAllLines(item);
+                books.Add(new Book()
+                {
+                    id = Int32.Parse(lines[0]),
+                    Author = lines[1],
+                    Price = Int32.Parse(lines[2]),
+                });
+            }
+            return View(books);
+        }
+
+        // GET: Detail
+        public ActionResult Detail(int id)
+        {   
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("~/App_Data/" + id + ".txt"));
+            Book book = new Book()
+            {
+                id = Int32.Parse(lines[0]),
+                Author = lines[1],
+                Price = Int32.Parse(lines[2]),
+            };
+            return View(book);
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            Book book = new Book();
+            return View(book);
+        }
+
+        // POST: Create
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            string content = $"{form["id"]}\n{form["author"]}\n{form["price"]}";
+            System.IO.File.WriteAllText(Server.MapPath("~/App_Data/" + form["id"] + ".txt"), content);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Edit
+        public ActionResult Edit(int id)
+        {
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("~/App_Data/" + id + ".txt"));
+            Book book = new Book()
+            {
+                id = Int32.Parse(lines[0]),
+                Author = lines[1],
+                Price = Int32.Parse(lines[2]),
+            };
+            return View(book);
+        }
+
+        // POST: Edit
+        [HttpPost]
+        public ActionResult Edit(FormCollection form)
+        {
+            string content = $"{form["id"]}\n{form["author"]}\n{form["price"]}";
+            System.IO.File.WriteAllText(Server.MapPath("~/App_Data/" + form["id"] + ".txt"), content);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Delete
+        public ActionResult Delete(int id)
+        {
+            string[] lines = System.IO.File.ReadAllLines(Server.MapPath("~/App_Data/" + id + ".txt"));
+            Book book = new Book()
+            {
+                id = Int32.Parse(lines[0]),
+                Author = lines[1],
+                Price = Int32.Parse(lines[2]),
+            };
+            return View(book);
+        }
+
+        //Post: Delete
+        [HttpPost]
+        public ActionResult Delete(FormCollection form)
+        {
+            string Id = form["id"];
+            string fn = Server.MapPath("~/App_Data/" + Id + ".txt");
+            if (System.IO.File.Exists(fn))
+            {
+                System.IO.File.Delete(fn);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ bookstore/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bookstore.Models
+{
+    public class Book
+    {
+        public int id;
+        public string Author;
+        public int Price;
+
+        public Book()
+        {
+
+        }
+        public Book(int _id, string author, int price)
+        {
+            id = _id;
+            Author = author;
+            Price = price; 
+        }
+    }
+}
